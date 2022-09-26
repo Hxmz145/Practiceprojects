29fee1984ba2c62ca8acfad8a9e4da6d0f80e532
Hamza Ahmed <93055724+Hxmz145@users.noreply.github.com>
2022-09-26 08:43:40 -0600

    Add files via upload

---
+++ Cryptography2.py
@@ -0,0 +1,21 @@
+Plaintext_input = (input("Enter a lelter to shift in ceaser cypher:"))
+S = (int(input("enter shift pattern you would like:")))
+new_list = [*Plaintext_input]
+encrypt_list = []
+for i in range(len(new_list)):
+    new_list1 = ord(new_list[i])
+    crpyt = new_list1+S
+    if crpyt > 122:
+        crpyt = 95+S
+    encrypt = chr(crpyt)
+    encrypt_list.append(encrypt)
+    cipher = ''.join(encrypt_list)
+    
+               
+
+
+
+print("Plain text :" + Plaintext_input)
+print("Shift pattern: " + str(S))
+print("Cipher:",cipher)
+
